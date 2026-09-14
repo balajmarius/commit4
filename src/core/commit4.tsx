5f6aa421c3a6f66084b9d0a6a9f8cefa29fa8773
Balaj Marius <balajmarius@gmail.com>
2026-09-14 16:31:30 +0300

    Use viewport height for the game case

--- src/core/commit4.tsx
+++ src/core/commit4.tsx
@@ -4,7 +4,7 @@ import { Scene } from "@/core/scene";
 
 export const Commit4 = () => {
   return (
-    <div className="relative mx-auto my-8 flex h-235 min-h-screen w-166 items-start justify-center after:absolute after:inset-0 after:bg-case after:bg-no-repeat after:drop-shadow-xl">
+    <div className="relative mx-auto my-8 flex h-screen w-166 items-start justify-center after:absolute after:inset-0 after:bg-case after:bg-no-repeat after:drop-shadow-xl">
       <div className="relative mt-23 after:absolute after:inset-0 after:bg-screen after:bg-no-repeat">
         <Suspense>
           <Scene />
