acee06b886b37c8063c8bed42ea7c444c83be502
Balaj Marius <balajmarius@gmail.com>
2026-09-17 11:52:33 +0100

    feat: add idle LCD background and optimize PNG assets

--- src/core/scene.tsx
+++ src/core/scene.tsx
@@ -19,12 +19,7 @@ export const Scene = () => {
   const atlas = useAtlas();
 
   return (
-    <Application
-      width={SCENE_WIDTH}
-      height={SCENE_HEIGHT}
-      backgroundAlpha={0}
-      className="bg-(--color-screen)"
-    >
+    <Application width={SCENE_WIDTH} height={SCENE_HEIGHT} backgroundAlpha={0}>
       <pixiSprite texture={atlas.textures.environment} />
       <Bugs />
       <Bullets />
